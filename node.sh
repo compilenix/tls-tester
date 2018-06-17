d96280e4910b884f300178f79aa2686aeeb600de
Compilenix <compilenix@gmail.com>
2018-06-18 01:41:37 +0200

    update node.sh to work with symlinks

--- node.sh
+++ node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 unset npm_config_prefix
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$(realpath $HOME/.nvm)"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" || {
   echo "you need nvm (https://github.com/creationix/nvm)"; exit 1
 }
