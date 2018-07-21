44db7461bb2cd271b6c1f187cec676b1cef7d54f
Compilenix <compilenix@gmail.com>
2018-07-21 13:58:47 +0200

    remove unused code

--- tlsinfo/index.js
+++ tlsinfo/index.js
@@ -2,27 +2,6 @@
 const { ServiceAudit } = require('./lib/ServiceAudit')
 const { Certificate } = require('./lib/Certificate')
 
-// async function run () {
-//   try {
-//     const cert = new Certificate({
-//       host: 'google.com',
-//       port: 465
-//     })
-//     let certResult = null
-//     try { certResult = await cert.get(2500) } catch (error) { }
-//     debugger
-//     cert.setOptions({
-//       host: 'heise.de'
-//     })
-//     try { certResult = await cert.get() } catch (error) { }
-//     debugger
-//   } catch (error) {
-//     debugger
-//   }
-// }
-
-// run()
-
 module.exports = {
   ServiceAudit: ServiceAudit,
   Certificate: Certificate
