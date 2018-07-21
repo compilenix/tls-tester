f0745dc7a79589352251575807302a7582b5bc6b
Compilenix <compilenix@gmail.com>
2018-07-21 20:46:14 +0200

    WIP: update tlsinfo type definitions

--- local-types/tlsinfo/index.d.ts
+++ local-types/tlsinfo/index.d.ts
@@ -7,6 +7,47 @@
 declare module 'tlsinfo' {
   import { X509, TlsProtocol, Cipher } from 'x509'
   import { ConnectionOptions, TLSSocket } from 'tls'
+  import { Socket } from 'net'
+
+  export class TimeOutableSocket extends NodeJS.EventEmitter {
+    /**
+     * Default: 30000 -> 30s
+     */
+    readonly timeout: number
+    protected readonly socket: Socket
+
+    constructor(socket: Socket)
+    constructor(socket: Socket, timeout: number)
+    destroySocket(): void
+    destroySocket(error: any): void
+    setSocket(socket: Socket): void
+    /**
+     * default 30000ms -> 30s
+     */
+    setTimeout(ms: number): void
+    on(event: 'timeout', listener: (...args: any[]) => void): this
+    emit(event: 'timeout', ...args: any[]): boolean
+  }
+
+  export class TlsSocketWrapper extends TimeOutableSocket {
+    protected readonly socket: TLSSocket
+
+    constructor()
+    constructor(options: ConnectionOptions)
+    private onTimeout(): void
+    private onError(error: any): void
+    private onError(error: any, reject: (reason?: any) => void): void
+    /**
+     * default 30000ms -> 30s
+     */
+    setOptions(options: ConnectionOptions): void
+    resetOptions(): void
+    resetOptions(options: ConnectionOptions): void
+    setNoDelay(noDelay?: boolean): this;
+    setKeepAlive(enable?: boolean, initialDelay?: number): this;
+    connect(): Promise<TLSSocket>
+    connect(timeout: number): Promise<TLSSocket>
+  }
 
   export interface CertificateResult {
     host: string
@@ -17,44 +58,6 @@ declare module 'tlsinfo' {
     certCaPem: string
   }
 
-  export interface ServiceAuditResult {
-    host: string
-    port: number
-    cert: X509
-    certPEM: string
-    certCa?: X509
-    certCaPem?: string
-    protocols: TlsProtocol[]
-    ciphers: Cipher,
-    ignoreReports?: string[]
-  }
-
-  export class TlsSocketWrapper extends NodeJS.EventEmitter {
-    /**
-     * Default: 30000 -> 30s
-     */
-    readonly timeout: number
-    constructor()
-    constructor(options: ConnectionOptions)
-    destroySocket(): void
-    destroySocket(error: any): void
-    private onTimeout(): void
-    private onError(error: any): void
-    private onError(error: any, reject: (reason?: any) => void): void
-    /**
-     * default 30000ms -> 30s
-     */
-    setTimeout(ms: number): void
-    on(event: 'timeout', listener: (...args: any[]) => void): this
-    emit(event: 'timeout', ...args: any[]): boolean
-    setOptions(options: ConnectionOptions): void
-    resetOptions(): void
-    resetOptions(options: ConnectionOptions): void
-    setNoDelay(noDelay?: boolean): this;
-    setKeepAlive(enable?: boolean, initialDelay?: number): this;
-    connect(): Promise<TLSSocket>
-    connect(timeout: number): Promise<TLSSocket>
-  }
   export class Certificate extends TlsSocketWrapper {
     constructor()
     constructor(options: ConnectionOptions)
@@ -70,9 +73,138 @@ declare module 'tlsinfo' {
     fetch(timeout: number): Promise<CertificateResult>
   }
 
-  export class ProtocolVersion extends TlsSocketWrapper {
-
+  export interface CipherResult {
+    /**
+     * @see {Cipher.suites}
+     */
+    enabled: string[]
+    /**
+     * @see {Cipher.suites}
+     */
+    disabled: string[]
+    /**
+     * @see {Cipher.suites}
+     */
+    unsupported: string[]
   }
-  export class ServiceAudit extends TlsSocketWrapper {
+
+  export class Cipher extends TlsSocketWrapper {
+    static readonly suites: [
+      'NULL-MD5',
+      'NULL-SHA',
+      'EXP-RC4-MD5',
+      'RC4-MD5',
+      'RC4-SHA',
+      'EXP-RC2-CBC-MD5',
+      'IDEA-CBC-SHA',
+      'EXP-DES-CBC-SHA',
+      'DES-CBC-SHA',
+      'DES-CBC3-SHA',
+      'EXP-DHE-DSS-DES-CBC-SHA',
+      'DHE-DSS-CBC-SHA',
+      'DHE-DSS-DES-CBC3-SHA',
+      'EXP-DHE-RSA-DES-CBC-SHA',
+      'DHE-RSA-DES-CBC-SHA',
+      'DHE-RSA-DES-CBC3-SHA',
+      'EXP-ADH-RC4-MD5',
+      'ADH-RC4-MD5',
+      'EXP-ADH-DES-CBC-SHA',
+      'ADH-DES-CBC-SHA',
+      'ADH-DES-CBC3-SHA',
+      'EXP1024-DES-CBC-SHA',
+      'EXP1024-RC4-SHA',
+      'EXP1024-DHE-DSS-DES-CBC-SHA',
+      'EXP1024-DHE-DSS-RC4-SHA',
+      'DHE-DSS-RC4-SHA'
+      // TODO: complete list
+    ]
+
+    constructor()
+    constructor(options: ConnectionOptions)
+    /**
+     * Similar to setTimeout but does apply to each individual connection rather then to the whole service audit.
+     *
+     * Default: equal to setTimeout
+     */
+    setTimeoutPerConnection(ms: number): void
+    test(): Promise<ProtocolVersionResult>
+    test(timeout: number): Promise<ProtocolVersionResult>
+    test(timeoutPerConnection: number): Promise<ProtocolVersionResult>
+  }
+
+  export interface ProtocolVersionResult {
+    /**
+     * @see {ProtocolVersion.protocols}
+     */
+    enabled: string[]
+    /**
+     * @see {ProtocolVersion.protocols}
+     */
+    disabled: string[]
+    /**
+     * @see {ProtocolVersion.protocols}
+     */
+    unsupported: string[]
+  }
+
+  export class ProtocolVersion extends TlsSocketWrapper {
+    static readonly protocols: [
+      'SSLv2',
+      'SSLv3',
+      'TLSv1',
+      'TLSv1.1',
+      'TLSv1.2',
+      'TLSv1.3'
+    ]
+
+    constructor()
+    constructor(options: ConnectionOptions)
+    /**
+     * @param protocol I.e.: TLSv1.2
+     */
+    protected static map(protocol: 'SSLv2' | 'SSLv3' | 'TLSv1' | 'TLSv1' | 'TLSv1_1' | 'TLSv1_2' | 'TLSv1_3'): 'SSLv2_method' | 'SSLv3_method' | 'TLSv1_method' | 'TLSv1_1_method' | 'TLSv1_2_method' | 'TLSv1_3_method' | ''
+    static getSupportedProtocols(): [] | [
+      'SSLv2',
+      'SSLv3',
+      'TLSv1'
+    ] | [
+      'SSLv2',
+      'SSLv3',
+      'TLSv1',
+      'TLSv1.1',
+      'TLSv1.2'
+    ] | [
+      'SSLv3',
+      'TLSv1',
+      'TLSv1.1',
+      'TLSv1.2'
+    ] | [
+      'SSLv3',
+      'TLSv1',
+      'TLSv1.1',
+      'TLSv1.2',
+      'TLSv1.3'
+    ]
+    test(protocol: 'SSLv2' | 'SSLv3' | 'TLSv1' | 'TLSv1' | 'TLSv1_1' | 'TLSv1_2' | 'TLSv1_3'): Promise<ProtocolVersionResult>
+    test(protocol: 'SSLv2' | 'SSLv3' | 'TLSv1' | 'TLSv1' | 'TLSv1_1' | 'TLSv1_2' | 'TLSv1_3', timeout: number): Promise<ProtocolVersionResult>
+    test(protocol: 'SSLv2' | 'SSLv3' | 'TLSv1' | 'TLSv1' | 'TLSv1_1' | 'TLSv1_2' | 'TLSv1_3', timeoutPerConnection: number): Promise<ProtocolVersionResult>
+  }
+
+  export interface ServiceAuditResult {
+    certificate: CertificateResult
+  }
+
+  export class ServiceAudit extends TimeOutableSocket {
+    constructor()
+    constructor(options: ConnectionOptions)
+    /**
+     * Similar to setTimeout but does apply to each individual connection rather then to the whole service audit.
+     *
+     * Default: equal to setTimeout
+     */
+    setTimeoutPerConnection(ms: number): void
+    run(): Promise<ServiceAuditResult>
+    run(timeout: number): Promise<ServiceAuditResult>
+    run(timeoutPerConnection: number): Promise<ServiceAuditResult>
   }
 }
